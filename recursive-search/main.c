807496759a41710d6da936e0f533ea804bce81cb
TaRaKaN41K <FVVMS13.2017@gmail.com>
2023-04-21 14:20:22 +0300

    Initial Commit

---
+++ recursive-search/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  recursive-search
+//
+//  Created by Феодор Калашов on 21.04.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
